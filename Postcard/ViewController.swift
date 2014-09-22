caf3e47a74ff807417554d46e05b87186005cd20
Terence Billingsley <terencebillingsley@MacBook-Air.local>
2014-09-22 16:45:48 -0400

    My first completed Version

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    @IBOutlet weak var enterNameTextField: UITextField!
+    
+    @IBOutlet weak var enterAMessageTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+   @IBAction func sendMailButtonPressed(sender: UIButton) {
+        //code will evaluate when we press the button
+        
+        messageLabel.hidden = false
+        messageLabel.text = enterAMessageTextField.text
+        enterAMessageTextField.text = ""
+        enterNameTextField.text = ""
+        
+        //now call a function to hide the keyboard once we've selected the button
+        
+        enterAMessageTextField.resignFirstResponder()
+        
+        //now we'll set the message content to red
+        
+        messageLabel.textColor = UIColor.redColor()
+        
+}
 }
 
